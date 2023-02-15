5765053e22f5dab8a1575165d6de5ccbd9daa04d
Alex <alexandroff1997@gmail.com>
2023-02-16 02:27:46 +0700

    implementation of controller and route logic for posts

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -1,5 +1,17 @@
-class Post {
+const db = require('../db/db.js');
 
+class Post {
+	async createPost(req, res) {
+		const { title, content, personId } = req.body;
+		const newPost = await db.query(`INSERT INTO post (title, content, person_id) VALUES ($1, $2, $3) RETURNING *`, [title, content, personId]);
+		res.json(newPost.rows[0]); 
+	}
+
+	async getPostsByUser(req, res) {
+		const id = req.query.id;
+		const posts = await db.query(`SELECT * FROM post WHERE person_id = $1`, [id]);
+		res.json(posts.rows);
+	}
 }
 
 module.exports = new Post();
